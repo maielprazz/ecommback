190f66c4e967301082a1181a57883d84def19d1d
Ismail <maiels.kun@gmail.com>
2021-05-28 09:38:18 +0700

    last update

--- base/serializers.py
+++ base/serializers.py
@@ -2,7 +2,13 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Product
 
+class UserSerializer(serializers.ModelSerializer):
+ class Meta:
+   model = Product
+   fields = '__all__'
+
 class ProductSerializer(serializers.ModelSerializer):
  class Meta:
    model = Product
    fields = '__all__'
+
